ec2ef552d645c5f79b9a59996b7933169a8680a0
Yeonju Jo <yjj0287@gmail.com>
2024-01-02 15:24:23 +0900

    config: next config 수정

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -2,10 +2,9 @@
 
 const isBuildCommand = process.env.npm_lifecycle_event === 'build'
 const debug = process.env.NODE_ENV !== 'production'
-const repository = 'https://yeonju0110.github.io/yeonju'
+const repository = 'yeonju'
 
 const nextConfig = {
-  basePath: '/yeonju',
   output: isBuildCommand ? 'export' : undefined,
   assetPrefix: !debug ? `/${repository}/` : '',
   trailingSlash: true,
