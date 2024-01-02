2f9824e79e484450171c705e4120fcd87bad521b
Yeonju Jo <yjj0287@gmail.com>
2024-01-02 16:32:02 +0900

    config: local assetPrefix 수정

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 const isBuildCommand = process.env.npm_lifecycle_event === 'build'
-const debug = process.env.NODE_ENV !== 'production'
+const isLocal = process.env.IM === 'local'
 const repository = 'yeonju'
 
 const nextConfig = {
   output: isBuildCommand ? 'export' : undefined,
-  assetPrefix: !debug ? `/${repository}/` : '',
+  assetPrefix: !isLocal ? `/${repository}/` : '',
   trailingSlash: true,
 }
 
