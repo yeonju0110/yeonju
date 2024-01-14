7509c70b6f3499cdd71f75226ac45eeeb744a617
Yeonju Jo <yjj0287@gmail.com>
2024-01-14 18:05:54 +0900

    config: sass, svg config 설정

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-
+const path = require('path')
 const isBuildCommand = process.env.npm_lifecycle_event === 'build'
 const isLocal = process.env.IM === 'local'
 const repository = 'yeonju'
@@ -8,6 +8,18 @@ const nextConfig = {
   output: isBuildCommand ? 'export' : undefined,
   assetPrefix: !isLocal ? `/${repository}/` : '',
   trailingSlash: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "@/styles/abstracts/variables";`,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 }
 
 module.exports = nextConfig
