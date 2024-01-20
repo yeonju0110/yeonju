d428a6f957df1a979ad9b9f1971947cf632b5467
Yeonju Jo <yjj0287@gmail.com>
2024-01-20 16:03:19 +0900

    feat: footer component 제작

---
+++ apps/blog/src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.scss'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      Copyright 2024. Jo Yeonju. All Rights Reserved.
+    </footer>
+  )
+}
+
+export default Footer
