09169aa51ad058f554978d927176c587fb74012a
Yeonju Jo <yjj0287@gmail.com>
2024-01-20 16:03:44 +0900

    remove: image component 폴더 이동으로 인한 제거

--- apps/blog/src/components/common/Image.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import NextImage, { ImageProps } from 'next/image'
-import defaultImageLoader from '@/services/imageLoader'
-
-export default function Image({ ...props }: ImageProps) {
-  return <NextImage loader={defaultImageLoader} {...props} />
-}
